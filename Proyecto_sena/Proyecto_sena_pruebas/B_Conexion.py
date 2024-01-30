dd47b751119552c61fc01c443d13d8fbe18f3737
malkings <141158744+malkings@users.noreply.github.com>
2024-01-30 07:43:33 -0500

    apartado de desarrollo en el rol de aprendiz

---
+++ Proyecto_sena/Proyecto_sena_pruebas/B_Conexion.py
@@ -0,0 +1,10 @@
+from A_Imports import *
+#---------------------------------------------------
+
+#B_Conexion contiene la conexion a la base de datos
+
+
+
+
+#La siguiente funcion sirve para comprobar
+#la conexion con la base de datos de manera exitosa
